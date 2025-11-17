5f3b9a6cc3ef0e253fb52625493cdb5a4944efdc
Rafael Avelar Batista De Jesus <avela019@ahl04.agate.msi.umn.edu>
2025-11-16 20:05:44 -0600

    removed excess loss functions floating around

--- src/reference_net_mod01.py
+++ src/reference_net_mod01.py
@@ -359,63 +359,7 @@ class NvNet_MOD01(nn.Module):
         
         return out_end
     
-    
-    
-from torch.nn.modules.loss import _Loss
 
-
-class SoftDiceLoss(_Loss):
-    '''
-    Soft_Dice = 2*|dot(A, B)| / (|dot(A, A)| + |dot(B, B)| + eps)
-    eps is a small constant to avoid zero division, 
-    '''
-    def __init__(self, *args, **kwargs):
-        super(SoftDiceLoss, self).__init__()
-
-    def forward(self, y_pred, y_true, eps=1e-8):
-        intersection = torch.sum(torch.mul(y_pred, y_true)) 
-        union = torch.sum(torch.mul(y_pred, y_pred)) + torch.sum(torch.mul(y_true, y_true)) + eps
-
-        dice = 2 * intersection / union 
-        dice_loss = 1 - dice
-
-        return dice_loss
-
-
-class CustomKLLoss(_Loss):
-    '''
-    KL_Loss = (|dot(mean , mean)| + |dot(std, std)| - |log(dot(std, std))| - 1) / N
-    N is the total number of image voxels
-    '''
-    def __init__(self, *args, **kwargs):
-        super(CustomKLLoss, self).__init__()
-
-    def forward(self, mean, std):
-        return torch.mean(torch.mul(mean, mean)) + torch.mean(torch.mul(std, std)) - torch.mean(torch.log(torch.mul(std, std))) - 1
-
-
-class CombinedLoss_ref(_Loss):
-    '''
-    Combined_loss = Dice_loss + k1 * L2_loss + k2 * KL_loss
-    As default: k1=0.1, k2=0.1
-    '''
-    def __init__(self, k1=0.1, k2=0.1):
-        super(CombinedLoss_ref, self).__init__()
-        self.k1 = k1
-        self.k2 = k2
-        self.dice_loss = SoftDiceLoss()
-        self.l2_loss = nn.MSELoss()
-        self.kl_loss = CustomKLLoss()
-
-    def forward(self, seg_y_pred, seg_y_true, rec_y_pred, rec_y_true, y_mid):
-        est_mean, est_std = (y_mid[:, :128], y_mid[:, 128:])
-        dice_loss = self.dice_loss(seg_y_pred, seg_y_true)
-        l2_loss = self.l2_loss(rec_y_pred, rec_y_true)
-        kl_div = self.kl_loss(est_mean, est_std)
-        combined_loss = dice_loss + self.k1 * l2_loss + self.k2 * kl_div
-        #print("dice_loss:%.4f, L2_loss:%.4f, KL_div:%.4f, combined_loss:%.4f"%(dice_loss,l2_loss,kl_div,combined_loss))
-        
-        return combined_loss
     
 # Handling undivisible sizes
 def make_divisible_crop_or_pad(shape, s):
@@ -513,27 +457,6 @@ def make_crop_or_pad(in_shape, out_shape):
 
     return transform
 
-#Config parameters for reference network
-
-# Number of Input Channels
-inChans = 4
-
-# Input Image Shape
-input_shape = (1,144, 240, 240)
-
-# Number of Output Classes
-seg_outChans = 3
-
-# The Activation Function to be used in the NVNet 
-# options: [relu, elu]
-activation = "relu"
-
-# The Type of Normalization to be used in the NVNet 
-# options: [group_normalization]
-normalization = "group_normalization"
-
-# Whether to use VAE in the encoder-decoder network
-VAE_enable = True
 
 # Local Test
 if __name__ == "__main__":
