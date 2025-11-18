3fefd594238c777f9b0d155f31b06f4cc07c884f
Justin Bueno <justinbueno68@gmail.com>
2025-11-18 14:27:38 -0600

    default training parameters set up

--- config.py
+++ config.py
@@ -3,18 +3,19 @@ import numpy as np
 
 #Choose parameters
 class Training_Parameters:
-    def __init__(self):
+    def __init__(self, net = "REF", VAE_enable = True, num_epochs = 300, LR = 1e-4, batch = 1, degradation_type = 'downsampling',
+                 downsamp_type = 'bilinear', ds_ratio = 1):
         
         #Choose a network
         possible_nets = ["REF", "MOD_01", "MOD_02", "MOD_03"]
         
-        self.net = "MOD_01"
-        self.VAE_enable = True
+        self.net = net
+        self.VAE_enable = VAE_enable
 
         #Basic parameters for training
-        self.num_epochs = 300      
-        self.learning_rate = 1e-4
-        self.batch_size = 1
+        self.num_epochs = num_epochs    
+        self.learning_rate = LR
+        self.batch_size = batch
         self.train_ratio = 0.8              #What ratio of dataset for training (Training ratio = 1 - validation ratio)
         self.validation = True              #Whether you want validation each epoch
         self.save_model_each_epoch = True   #Save model and training parameters every epoch
@@ -22,9 +23,9 @@ class Training_Parameters:
         
         
         #Choose which type of downsampling/degradation
-        self.degradation_type = 'downsampling'
-        self.downsamp_type = 'bilinear'  #Type of downsampling
-        self.ds_ratio = 2                #Downsampling factor (if doing downsamplin at all)
+        self.degradation_type = degradation_type
+        self.downsamp_type = downsamp_type  #Type of downsampling
+        self.ds_ratio = ds_ratio              #Downsampling factor (if doing downsamplin at all)
         self.HR_layers = np.log2(self.ds_ratio)
         
         
