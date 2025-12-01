6215b8934d6f93841d8963a1764e698b7cbbd770
rafa204 <avela019@umn.edu>
2025-11-30 22:35:20 -0600

    Added params for 2D vs 3D networks

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ def parse_args():
     parser.add_argument("--degradation_type", type=str, default="downsampling",
                         help="Degradation type (default: downsampling)")
 
-    parser.add_argument("--downsamp_type", type=str, default="bilinear",
+    parser.add_argument("--downsamp_type", type=str, default="wavelet",
                         help="Downsampling type (default: bilinear)")
 
     parser.add_argument("--ds_ratio", type=int, default=1,
@@ -52,12 +52,15 @@ def parse_args():
     parser.add_argument("--UNET_disable", action="store_false", dest="UNET_enable")
     parser.set_defaults(UNET_enable=True)
 
+    parser.add_argument("--VAE_warmup", action="store_true")
+    parser.set_defaults(VAE_warmup=False)
+
     return parser.parse_args()
 
 #Choose parameters
 class Training_Parameters:
-    def __init__(self, net = "REF", VAE_enable = True, num_epochs = 300, LR = 1e-4, batch = 1, degradation_type = 'downsampling',
-                 downsamp_type = 'bilinear', ds_ratio = 1, crop = True):
+    def __init__(self, net = "REF", VAE_enable = True, UNET_enable = True, num_epochs = 300, LR = 1e-4, batch = 1, degradation_type = 'downsampling',
+                 downsamp_type = 'bilinear', ds_ratio = 1, crop = True, VAE_warmup = False, fusion = "None"):
         
         #Choose a network
         possible_nets = ["REF", "MOD_01", "MOD_02", "MOD_03", "REF_US", "VAE_M01", "VAE_M04", "VAE_2D"]
@@ -65,12 +68,15 @@ class Training_Parameters:
 
         self.net = net
         self.VAE_enable = VAE_enable
+        self.UNET_enable = UNET_enable
+        self.VAE_warmup = VAE_warmup
+        self.fusion = fusion
 
         #Basic parameters for training
         self.num_epochs = num_epochs    
         self.learning_rate = LR
         self.batch_size = batch
-        self.train_ratio = 0.8              #What ratio of dataset for training (Training ratio = 1 - validation ratio)
+        self.train_ratio = 0.5            #What ratio of dataset for training (Training ratio = 1 - validation ratio)
         self.validation = True              #Whether you want validation each epoch
         self.save_model_each_epoch = True   #Save model and training parameters every epoch
         self.crop = crop
@@ -82,14 +88,13 @@ class Training_Parameters:
         self.HR_layers = np.log2(self.ds_ratio)
         
         
-        
         #Data preparation parameters (choose dataset size as slabs_per_volume * num_volumes)
-        self.threeD = True #Use volume dimension
+        self.threeD = self.net in ["REF", "MOD_01", "MOD_02", "MOD_03", "REF_US"] #Use volume dimension
         self.slab_dim = 144
         self.slabs_per_volume = 1
-        self.num_volumes = 369  #Maximum = 369 for the training dataset
+        self.num_volumes = 2  #Maximum = 369 for the training dataset
         self.data_shape = [240,240,155] #Original data shape [Height x Width x Depth]
-        self.crop_size = [144,240,240] #Used data shape [Depth x Height x Width]
+        self.crop_size = [self.slab_dim,240,240] #Used data shape [Depth x Height x Width]
         self.modality_index = 0 #If single modality, which one to choose
         self.augment = True     #Perform data augmentation or not
         self.binary_mask = False 
